b74f221e15bfdc5a490420719369a04aef32fc70
Zainul Abedin <zabedin@hyperiongrp.com>
2020-09-10 09:27:56 +0530

    Added Code for basic Crud and a middleware.

---
+++ src/DevAl.Play.GroupManage.Web/Controllers/GroupsController.cs
@@ -0,0 +1,76 @@
+﻿using DevAl.Play.GroupManage.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevAl.Play.GroupManage.Web.Controllers
+{
+    [Route("Groups")]
+    public class GroupsController : Controller
+    {
+       static List<GroupViewModel> groups = new List<GroupViewModel> { new GroupViewModel { Id = 1, Name = "Man UTD" } };
+        [Route("/")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(groups);
+        }
+
+        [HttpGet]
+        [Route("/Details")]
+        public IActionResult Details(int id)
+        {
+            GroupViewModel group = groups.SingleOrDefault(r => r.Id == id);
+            return View(group);
+        }
+
+        [Route("/Add")]
+        //[Route("/Edit/{id?}")]
+        [HttpGet]
+        public IActionResult Add(int id)
+        {
+            GroupViewModel group = groups.SingleOrDefault(r => r.Id == id);
+            if(group != null)
+            {
+                ViewBag.Title = "Edit";
+                return View(group);
+            }
+            else
+            {
+                ViewBag.Title = "Add";
+                return View(new GroupViewModel());
+            }
+        }
+
+        [HttpPost]
+        [Route("/Add")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(GroupViewModel group)
+        {
+            GroupViewModel g = groups.Skip(groups.Count - 1).LastOrDefault();
+            group.Id = g == null ? 1 : g.Id + 1;
+            groups.Add(group);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("/Edit")]
+        public IActionResult Edit(GroupViewModel group)
+        {
+            groups.FirstOrDefault(r => r.Id == group.Id).Name = group.Name;
+            return RedirectToAction("Index");
+        }
+
+        // [HttpPost]
+        [Route("/Remove")]
+        public IActionResult Remove(int id)
+        {
+            groups.Remove(groups.FirstOrDefault(r => r.Id == id));
+            return RedirectToAction("Index");
+        }
+    }
+}
--- src/DevAl.Play.GroupManage.Web/Startup.cs
+++ src/DevAl.Play.GroupManage.Web/Startup.cs
@@ -16,8 +16,11 @@ namespace DevAl.Play.GroupManage.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => { 
+            options.EnableEndpointRouting = false;
+            });
         }
-
+ 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -26,15 +29,20 @@ namespace DevAl.Play.GroupManage.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.Use(async (context, next) => {
+            context.Response.OnStarting(() => {
+                context.Response.Headers.Add("PP","Zain");
+                return Task.CompletedTask;
             });
+                await next.Invoke();
+            });
+            //app.UseRouting();
+            app.UseMvc();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapDefaultControllerRoute();
+            //});
+           
         }
     }
 }
